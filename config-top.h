957357f9a2227a8b8f8251e5de2cce66831e3f1b
Koro <koro@kzxiv.net>
2017-05-31 05:23:22 -0400

    Change default PS1.

--- config-top.h
+++ config-top.h
@@ -84,7 +84,7 @@
 #endif
 
 /* Default primary and secondary prompt strings. */
-#define PPROMPT "\\s-\\v\\$ "
+#define PPROMPT "[\\u@\\h \\W]\\$ "
 #define SPROMPT "> "
 
 /* Undefine this if you don't want the ksh-compatible behavior of reprinting
